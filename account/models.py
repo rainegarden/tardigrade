8fe60faf33285f1d9a2525241e1dfff48e2988c5
raine <raine@umd.edu>
2025-07-06 17:55:27 -0400

    Nixing using pfp object, just using gravitar

--- account/models.py
+++ account/models.py
@@ -1,11 +1,3 @@
 import uuid
 from django.contrib.auth.models import User
 from django.db import models
-
-
-class pfp(models.Model):
-    """
-Profile picture model for storing user profile images
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image_binary = models.BinaryField()
