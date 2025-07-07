53d8ad974b4d4af92c63f0da988d0a10fe4e5034
raine <raine@umd.edu>
2025-07-07 16:11:35 -0400

    Threw in bootstrap icons

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    # Relation stuff
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True)
+    children = models.ManyToManyField('self', blank=True, related_name='parent')
+    level = models.IntegerField(default=1)
+
+    # Name, Logo
+    name = models.CharField(max_length=100)
+    logo = models.CharField(max_length=100)
